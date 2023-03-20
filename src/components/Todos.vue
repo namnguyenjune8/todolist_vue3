5175af7878f99444441f903a2a49feecf9782a30
Nguyen Hoang Nam <namnguyenjune8@gmail.com>
2023-03-20 16:25:03 +0700

    Update Todos.vue

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,58 +1,57 @@
-<template>
-  <div>
-    <div class="todo-list">
-      <TodoItem v-for="task in todos" 
-        :key="task._id" 
-        :taskID="task.id" 
-        :todos="todos"
-        @itemcompleted="markComplete" 
-        @deleteItem ="deleteTodo"/>
+
+  <template>
+    <div>
+      <div class="todo-list">
+        <div v-for="task in todos" :key="task._id">
+          <TodoItem :task="task" />
+          <input type="checkbox" :checked="task.completed" @change="markComplete(task._id)">
+          <button @click="deleteTodo(task.id)">Delete</button>
+        </div>
+      </div>
+      <a href="/sign-in">Đăng xuất</a>
     </div>
-    <a href="/sign-in">Đăng xuất</a>
-  </div>
-</template>
-
-<script> 
-import {ref} from 'vue'
-import TodoItem from './TodoItem'
-
-import axios from 'axios';
-
-export default {
+  </template>
+  <script> 
+  import {ref, onMounted} from 'vue'
+  import TodoItem from './TodoItem'
+  import axios from 'axios';
+  
+  export default {
     name: 'task',
     components: { 
-        TodoItem, 
+      TodoItem, 
     },
     setup() {
-        const todos = ref([]);
-        axios.get('http://localhost:3000/task')
-    .then(response => {
-        todos.value = response.data;
-    })
-    .catch(error => {
-        console.log(error);
+      const todos = ref([]);
+      const markComplete = (id) => {
+        todos.value = todos.value.map((todo) => {
+          if (todo.id === id) todo.completed = !todo.completed;
+          return todo;
+        });
+      }
+      onMounted(() => {
+      axios.get('http://localhost:3000/task')
+        .then(response => {
+          todos.value = response.data;
+        })
+        .catch(error => {
+          console.log(error);
+        });
     });
-    const markComplete = (id) => {
-      todos.value = todos.value.map((todo) => {
-        if (todo.id === id) todo.completed = !todo.completed;
-        return todo;
-      });
-    }
 
-        
     return {
-    todos,
-    markComplete,
-}
+      todos,
+      markComplete,
     }
-
+  }
 }
 </script>
 
-<style>
-.todo-list {
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-}
-</style>
+
+  <style>
+  .todo-list {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+  }
+  </style>
