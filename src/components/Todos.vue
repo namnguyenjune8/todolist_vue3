5c2ccc48972b4c676bb07d3c90efee06f3b76ab1
Nguyen Hoang Nam <namnguyenjune8@gmail.com>
2023-03-20 14:42:55 +0700

    .

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <!-- <AddTodo  @add-todo="addTodo"/> -->
     <div class="todo-list">
       <TodoItem v-for="task in todos" 
         :key="task._id" 
@@ -17,14 +16,12 @@
 import {ref} from 'vue'
 import TodoItem from './TodoItem'
 
-// import AddTodo from './AddTodo'
 import axios from 'axios';
 
 export default {
     name: 'task',
     components: { 
         TodoItem, 
-        // AddTodo
     },
     setup() {
         const todos = ref([]);
@@ -41,31 +38,11 @@ export default {
         return todo;
       });
     }
-// //    const deleteTodo = async (id) => {
-//   try {
-//     await axios.delete(`http://localhost:3000/task/${id}`);
-//     todos.value = todos.value.filter((todo) => todo.id !== id);
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
-//     const addTodo = async (newTodo) => {
-//     try {
-//         const response = await axios.post('http://localhost:3000/task', {
-//             task: newTodo.title,
-//             completed: newTodo.completed,
-//     })
-//             todos.value.push(response.data)
-//     } catch (error) {
-//         console.log(error)
-// }
-// }
+
         
     return {
     todos,
     markComplete,
-    // deleteTodo,
-    // addTodo,
 }
     }
 
--- src/router/task.js
+++ src/router/task.js
@@ -32,45 +32,5 @@ router.get('/:id', getTask, (req, res) => {
   res.json(req.task)
 })
 
-// Tạo 1 task
-// router.post('/', async (req, res) => {
-//   const task = new Task({
-//     title: req.body.task,
-//     completed: req.body.completed
-//   });
-//   try {
-//     const newTask = await task.save();
-//     res.status(201).json(newTask);
-//   } catch (err) {
-//     res.status(400).json({ message: err.message });
-//   }
-// });
-
-// // Sửa 1 task
-// router.patch('/:id', getTask, async (req, res) => {
-//   if (req.body.title != null) {
-//     res.task.title = req.body.title;
-//   }
-//   if (req.body.completed != null) {
-//     res.task.completed = req.body.completed;
-//   }
-
-//   try {
-//     const updatedTask = await res.task.save();
-//     res.json(updatedTask);
-//   } catch (err) {
-//     res.status(400).json({ message: err.message });
-//   }
-// });
-
-//   // Xóa 1 task
-// router.delete('/:id', getTask, async (req, res) => {
-//     try {
-//       await res.task.remove();
-//       res.json({ message: 'Deleted Task' });
-//     } catch (err) {
-//       res.status(500).json({ message: err.message });
-//     }
-//   });
 
 module.exports = router;
